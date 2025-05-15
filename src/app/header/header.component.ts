f593129933e959180d0752155dc620a9cf3db238
Vishnu <vishnu.p@alldirectionsource.com>
2025-05-15 20:23:24 +0530

    Update perfomance

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
 
-  menuOpen = false;  
+  menuOpen = false;
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;  
+  navItems = [
+    { label: 'Home', route: '/home' },
+    { label: 'Product', route: '/product' },
+    { label: 'Contacts', route: '/contacts' },
+    { label: 'Location', route: '/location' },
+  ];
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  trackByLabel(index: number, item: any): string {
+    return item.label;
   }
 
 }
